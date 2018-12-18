d227c15a5dd0fbd9c689dd2d8601c38b18a01141
Joshua T <joshua@sonofone.net>
2018-12-18 11:09:44 -0600

    Removed env: prefix for BuildHelper variables

--- test/_Init.ps1
+++ test/_Init.ps1
@@ -1,7 +1,7 @@
 # Dot source this script in any Pester test script that requires the module to be imported.
 
 if (-not $SuppressImportModule) {
-    Write-Host "Importing module [[ $env:BHPSModuleManifest ]]" -ForegroundColor Cyan
-    Get-Module -Name $env:BHProjectName -ErrorAction SilentlyContinue | Remove-Module -Force
-    Import-Module $env:BHPSModuleManifest -Scope Global -Force
+    Write-Host "Importing module [[ $BHPSModuleManifest ]]" -ForegroundColor Cyan
+    Get-Module -Name $BHProjectName -ErrorAction SilentlyContinue | Remove-Module -Force
+    Import-Module $BHPSModuleManifest -Scope Global -Force
 }
