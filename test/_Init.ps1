808726600f4f87e733d7e6f0286eacaf6c9510f7
Joshua T <joshua@sonofone.net>
2020-07-08 16:09:27 -0500

    Remove "stub" tests

--- test/_Init.ps1
+++
@@ -1,7 +0,0 @@
-# Dot source this script in any Pester test script that requires the module to be imported.
-
-if (-not $SuppressImportModule) {
-    Write-Host "Importing module [[ $BHPSModuleManifest ]]" -ForegroundColor Cyan
-    Get-Module -Name $BHProjectName -ErrorAction SilentlyContinue | Remove-Module -Force
-    Import-Module $BHPSModuleManifest -Scope Global -Force
-}
